b92ab73ae42fc950430cab5b1ad954b3ccb7c282
Katherine Gomez <125319797+KLGomez@users.noreply.github.com>
2025-11-20 19:19:11 -0300

    fix: maps contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -170,7 +170,7 @@ export function Contact() {
             {/* Map */}
             <div className="rounded-lg overflow-hidden shadow-lg h-64 md:h-80">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.9098702899105!2d-58.36724682487601!3d-34.757863165654186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32d3140c79533%3A0xbcb112872b66c391!2sDr.%20Carlos%20Collivadino%20670%2C%20B1834INP%20Temperley%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1763420713180!5m2!1ses!2sar"
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6555.819356734265!2d-58.364672!3d-34.75786800000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32d3140c79533%3A0xbcb112872b66c391!2sDr.%20Carlos%20Collivadino%20670%2C%20B1834INP%20Temperley%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses!2sar!4v1763676937475!5m2!1ses!2sar"
                 width="100%"
                 height="100%"
                 style={{ border: 0 }}
